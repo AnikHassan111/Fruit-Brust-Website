cf73e885ad8eba7ae96655a8040dd1b0d09d81e8
Your NameAnikHassan111 <miaanik4@gmail.com>
2023-07-26 23:32:03 +0600

    Finish the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,4 @@ module.exports = {
     },
     plugins: [],
   }
-  // <div>
-  //         <!-- Card 1 -->
-  //         <div class="card w-96 bg-base-100 shadow-xl">
-  //           <figure><img src="images/fruit6.png" alt="Shoes" /></figure>
-  //           <div class="card-body">
-  //             <h2 class="text-2xl font-bold text-center">12.90$ - 18.85$</h2>
-  //             <p class="text-center">Apples</p>
-  //             <!-- Rating code Start -->
-  //             <div class="rating text-center">
-  //               <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
-  //               <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" checked />
-  //               <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
-  //               <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
-  //               <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
-  //             </div>
-  //             <!-- Rating code End -->
-  //           </div>
-  //         </div>
-  //       </div>
+  
